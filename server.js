105c5df862d2832d8545b5c8b8c32664269dcb25
pgartrell <preciousrg@outlook.com>
2021-12-14 07:32:48 -0500

    Setting up morgan middleware

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 //Since express is a core module we do not have to give a path it will look in node_modules automatically
 const express = require("express");
+const morgan = require('morgan'); //morgan logs req information
 
 const hostname = "localhost";
 const port = 3000;
 
 //This returns an express server application that will be available under this variable name
 const app = express();
+app.use(morgan('dev')); //configures morgan using the development version
+
+//Setting up express to serve files from public folder
+app.use(express.static(__dirname + '/public')) //__dirname refers to the absolute path of the current directory of the file that its in
 
 app.use((req, res) => {
-  console.log(req.headers);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
   res.end("<html><body><h1>This is an Express Server</h1></body></html>");
